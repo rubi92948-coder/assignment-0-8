ce08a9efe7cf66b103628666ff76700d304efb96
rubi92948 <rubi92948@gmail.com>
2026-05-04 20:43:00 +0600

    fix authClient production connection issue

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,8 +3,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL:
-    typeof window !== "undefined"
-      ? window.location.origin
-      : "",
+  baseURL: "", 
 });
