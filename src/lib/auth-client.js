10f979835a3b2427ae7dfd1d38914071c15d1df3
rubi92948 <rubi92948@gmail.com>
2026-05-04 22:05:04 +0600

    update auth-clint

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL:""
+  baseURL:"https://assignment-0-8-ne31-qn98ujvw8-rubi92948-coders-projects.vercel.app"
 })
 export const { signIn, signUp, useSession } = createAuthClient()
-
-
